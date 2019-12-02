37d9158bcb5cf0d23fa17908cf2ac523cff4f21c
Shadow_WolfDemon <thebrandonweidner@gmail.com>
2019-12-01 23:21:05 -0700

    Basic Setup
    Basic Setup of project folder | First INIT

---
+++ src/main/java/com/github.shadow-wolfdemon/pipes-pipes-pipes-/main.java
@@ -0,0 +1,13 @@
+public class main {
+
+    package com.github.shadow-wolfdemon.pipes-pipes-pipes-;
+
+import net.minecraftforge.fml.common.Mod;
+
+    @Mod(ExampleMod.MODID)
+    public final class ExampleMod {
+
+        public static final String MODID = "examplemod";
+
+    }
+}
